62cb79ef4fd2b98de829a31d4e949f202524b4ca
Szymon <jablonski.szymon21@gmail.com>
2026-05-09 15:24:37 +0200

    fix: change API path to /api

--- client/src/utils/fetchFunctions.ts
+++ client/src/utils/fetchFunctions.ts
@@ -3,7 +3,7 @@ import { paymentState } from "../store/payments/payments.types";
 import { product } from "../store/products/products.types";
 import { userCall, userData, userOrder, userPayload, userPutResponse, userUpdate } from "../store/user/user.types";
 
-export const serverUrl = "http://localhost:8000";
+export const serverUrl = "http://localhost:8000/api";
 
 type fetchType<T> = {
   status: string;
--- server/src/server.ts
+++ server/src/server.ts
@@ -12,13 +12,13 @@ app.use(
   cors({
     credentials: true,
     origin: "http://localhost:3000",
-  })
+  }),
 );
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
-app.use(api);
+app.use("/api", api);
 
 app.listen(process.env.PORT, async () => {
   console.log("Starting server...");
