79eb5cced17e5e79b7ff4c4d2c8191bdbf369f15
Szymon <jablonski.szymon21@gmail.com>
2026-05-09 14:54:05 +0200

    cdn

--- client/src/react-app-env.d.ts
+++ client/src/react-app-env.d.ts
@@ -1,2 +1,12 @@
 /// <reference types="react-scripts" />
 /// <reference types="redux-persist" />
+
+declare module "*.sass" {
+  const content: {};
+  export default content;
+}
+
+declare module "*.scss" {
+  const content: {};
+  export default content;
+}
